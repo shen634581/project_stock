166db683cc678d471fec935c0f2f4ee3954c5b89
shen <wa134679@gmail.com>
2025-02-10 01:17:39 +0800

    stock

---
+++ stock-update-api.php
@@ -0,0 +1,38 @@
+<?php
+// {"id":"xxx", "email": "owner@test.com"}
+
+// {"state" : true, "message" : "更新成功"}
+// {"state" : false, "message" : "更新失敗和錯誤代碼等"}
+// {"state" : false, "message" : "欄位不得為空白"}
+// {"state" : false, "message" : "欄位命名錯誤"}
+$data = file_get_contents("php://input", "r");
+$mydata = array();
+$mydata = json_decode($data, true);
+
+if (isset($mydata["id"]) && isset($mydata["remark"])){
+    if ($mydata["id"] != "" && $mydata["remark"] != "") {
+        $p_id = $mydata["id"];
+        $p_remark = $mydata["remark"];
+        require_once("dbtools.php");
+        $link = create_connection();
+
+        $sql = "UPDATE stock SET Remark = '$p_remark' WHERE Id = '$p_id'";
+
+       
+     
+        if( execute_sql($link, "stock", $sql)){
+            echo '{"state" : true,"message" : "更新成功"}';
+        }else{
+            
+            echo '{"state" : false, "message" : "更新失敗和錯誤代碼等"}';
+        }
+        mysqli_close($link);
+           
+       
+    } else {
+        echo '{"state" : false, "message" : "欄位不得為空白"}';
+    }
+} else {
+    echo '{"state" : false, "message" : "欄位命名錯誤"}';
+}
+?>
